6f3f8862ffc6c41aa66a63fbef9385098d2e9104
KornienkoVl <114586091+KornienkoVl@users.noreply.github.com>
2026-02-22 18:47:33 +0700

    Update V1__create_posts.sql

--- testovoe_lynx/src/main/resources/db/migration/V1__create_posts.sql
+++ testovoe_lynx/src/main/resources/db/migration/V1__create_posts.sql
@@ -5,4 +5,7 @@ CREATE TABLE posts (
                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                        com_counter INTEGER NOT NULL DEFAULT 0,
                        is_deleted BOOLEAN NOT NULL DEFAULT FALSE
+
 );
+CREATE INDEX idx_posts_created ON posts(is_deleted, created_at);
+CREATE INDEX idx_posts_comments ON posts(is_deleted, com_counter);
